8483e3619ee9506d83c2431e8e510dfee552e59e
Martin Frost <frost@ceri.se>
2021-12-03 21:24:16 +0100

    Add script for generating new day skeleton

---
+++ new_day.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3
+
+from pathlib import Path
+import os
+import sys
+from string import Template
+
+day_template = Template(
+    """
+from icecream import ic
+
+
+def part1(input):
+    ...
+
+
+def part2(input):
+    ...
+
+
+def read_input():
+    with open("input") as f:
+        f.readlines()
+
+
+if __name__ == "__main__":
+    input = read_input()
+    print(part1(input))
+    print(part2(input))
+"""
+)
+
+
+test_template = Template(
+    """
+from day$day import *
+
+
+test_input = []
+
+
+def test_part1():
+    assert part1(test_input) == 0, "Not yet implemented"
+
+
+def test_part2():
+    assert part2(test_input) == 0, "Not yet implemented"
+"""
+)
+
+
+if __name__ == "__main__":
+    day = sys.argv[1]
+
+    file_path = Path(__file__)
+    root_dir = file_path.parent
+    dirname = root_dir / f"day{day}"
+
+    os.mkdir(dirname)
+
+    with open(dirname / f"day{day}.py", "w") as f:
+        f.write(day_template.substitute())
+
+    with open(dirname / f"test_day{day}.py", "w") as f:
+        f.write(test_template.substitute({"day": day}))
